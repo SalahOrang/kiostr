6f78a47ab53d20c91d72f6d51606791dd817435f
DorikSetyawan <dorik655@gmail.com>
2020-01-10 02:06:12 +0700

    cok

--- index.php
+++ index.php
@@ -3,26 +3,19 @@
 $server_key = "SB-Mid-server-7_n_TLYkELFYzGo224UowxFd";
 
 $is_production = false;
-
 $api_url = $is_production ? 
-        'https://app.midtrans.com/snap/v1/transactions' :
-        'https://app.sandbox.midtrans.com/snap/v1/trasactions';
-
-
-        if (!strpos($_SERVER['REQUEST_URI'], '/charge')){
-    http_response_code(404);
-    echo "wrong path ,make it sure it's `/charge`";exit();
+  'https://app.midtrans.com/snap/v1/transactions' : 
+  'https://app.sandbox.midtrans.com/snap/v1/transactions';
+if( !strpos($_SERVER['REQUEST_URI'], '/charge') ) {
+  http_response_code(404); 
+  echo "wrong path, make sure it's `/charge`"; exit();
 }
-if($_SERVER['REQUEST_METHOD']!== 'POST'){
-    hhtp_response_code(404);
-    echo"Page not found";exit();
-
+if( $_SERVER['REQUEST_METHOD'] !== 'POST'){
+  http_response_code(404);
+  echo "Page not found or wrong HTTP request method is used"; exit();
 }
-$request_body= file_get_contents('php://input');
-header('Content-Type : application/json');
-
-$charge_result = chargeAPI($api_url, $server_key)
-
+$request_body = file_get_contents('php://input');
+header('Content-Type: application/json');
 $charge_result = chargeAPI($api_url, $server_key, $request_body);
 http_response_code($charge_result['http_code']);
 echo $charge_result['body'];
